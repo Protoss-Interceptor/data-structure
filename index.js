f231cdf1539a005fbaa608913b39abe3aae84c33
protoss interceptor <protoss.interceptor@gmail.com>
2022-05-02 23:25:17 +0900

    테스트 커밋

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Test = void 0;
+class Test {
+  constructor(_foo = 100) {
+    this.foo = _foo;
+  }
+}
+exports.Test = Test;
